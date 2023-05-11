e8a18c29d62394da73d6c4af55fa5d586bfeac26
Dekagor <dekagor@hotmail.com>
2023-05-11 00:08:29 -0300

    ultimando detalles

--- src/main/java/com/PortfolioPersonal/GRDK/Controller/CExperiencia.java
+++ src/main/java/com/PortfolioPersonal/GRDK/Controller/CExperiencia.java
@@ -28,8 +28,8 @@ import org.springframework.web.bind.annotation.RestController;
  */
 
 @RestController
-@RequestMapping("Explab")
-@CrossOrigin(origins = "http://localhost:4200")
+@RequestMapping("/Explab")
+@CrossOrigin(origins = {"https://porfront.web.app"})
 public class CExperiencia {
     @Autowired
     SExperiencia sExperiencia;
